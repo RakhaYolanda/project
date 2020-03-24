79a7911ffa5cd95e980792da4480546d4a58cf5b
RakhaYolanda <61130178+RakhaYolanda@users.noreply.github.com>
2020-03-24 11:19:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+   
+</head>
+<body id="page-top">
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top" id="mainNav">
+  <div class="container">
+  <a class="navbar-brand font-weight-bold text-white" href="#page-top">IJUP PLAYSTATION</a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link js-scroll-trigger text-white" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="#">Informasi</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Penyewaan
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Ps 1</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Ps 2</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Ps 3</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Ps 4</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <div class="container">
+  <h1 class="display-4">SELAMAT DATANG DI WEBSITE <br> <span class="font-weight-bold"> IJUP PLAYSTATION</span></h1>
+  <hr class="my-4">
+  <p class="lead">Website Penyewaan Playstation Terpercaya Se Indonesia</p>
+  <a class="btn btn-primary btn-lg text-dark font-weight-bold" href="#" role="button">KUNJUNGI</a>
+  </div>
+</div>
+
+</body>
+</html>
